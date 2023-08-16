db8b661fc1e56f7fae8c19a30c720263db9708a2
SilkyFowl <16532218+SilkyFowl@users.noreply.github.com>
2023-08-16 18:25:41 +0900

    initial commit.

---
+++ src/App/Program.fs
@@ -0,0 +1,93 @@
+﻿namespace App
+
+open Avalonia
+open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.Themes.Fluent
+open Avalonia.FuncUI.Hosts
+open Avalonia.Controls
+open Avalonia.FuncUI
+open Avalonia.FuncUI.DSL
+open Avalonia.Layout
+
+open Avalonia.FuncUI.LiveView
+open Avalonia.FuncUI.LiveView.Core.Types
+
+module Main =
+
+    [<LivePreview>]
+    let view () =
+        Component(fun ctx ->
+            let state = ctx.useState 0
+
+            DockPanel.create [
+                DockPanel.children [
+                    Button.create [
+                        Button.dock Dock.Bottom
+                        Button.onClick (fun _ -> state.Set(state.Current - 1))
+                        Button.content "-"
+                        Button.horizontalAlignment HorizontalAlignment.Stretch
+                        Button.horizontalContentAlignment HorizontalAlignment.Center
+                    ]
+                    Button.create [
+                        Button.dock Dock.Bottom
+                        Button.onClick (fun _ -> state.Set(state.Current + 1))
+                        Button.content "+"
+                        Button.horizontalAlignment HorizontalAlignment.Stretch
+                        Button.horizontalContentAlignment HorizontalAlignment.Center
+                    ]
+                    TextBlock.create [
+                        TextBlock.dock Dock.Top
+                        TextBlock.fontSize 48.0
+                        TextBlock.verticalAlignment VerticalAlignment.Center
+                        TextBlock.horizontalAlignment HorizontalAlignment.Center
+                        TextBlock.text (string state.Current)
+                    ]
+                ]
+            ])
+
+type MainWindow() =
+    inherit HostWindow()
+
+    do
+        base.Title <- "Counter Example"
+        base.Content <- Main.view ()
+
+module LiveView =
+    open System
+
+    let enabled =
+        match Environment.GetEnvironmentVariable("AVALONIA_FUNCUI_LIVEVIEW") with
+        | null -> false
+        | "1" -> true
+        | _ -> false
+
+type App() =
+    inherit Application()
+
+    override this.Initialize() =
+        this.Styles.Add(FluentTheme())
+        this.RequestedThemeVariant <- Styling.ThemeVariant.Dark
+
+    override this.OnFrameworkInitializationCompleted() =
+        match this.ApplicationLifetime with
+        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime ->
+            desktopLifetime.MainWindow <-
+                if LiveView.enabled then
+                    LiveViewWindow() :> Window
+                else
+                    MainWindow()
+        | _ -> ()
+
+#if DEBUG
+        this.AttachDevTools()
+#endif
+
+module Program =
+
+    [<EntryPoint>]
+    let main (args: string[]) =
+        AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .UseSkia()
+            .StartWithClassicDesktopLifetime(args)
